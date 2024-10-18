d7230e57b0eeb7013ca66ee83b3b77f48e913e7d
Ana Flavia Moreira Pires <flavia.pires@mail.uft.edu.br>
2024-10-17 21:20:14 -0300

    feat: :sparkles: coisas

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def main():
     gaussiano_mascara = [[1, 2, 1], [2, 4, 2], [1, 2, 1]]
     gaussiano_razao = 1 / 16
 
-    
+    media_mascara = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    media_razao = 1 / 9  
+
 
     img = Image.open("imagem/imagem.png")
     img = img.convert("L")
 
     resultado = aplicar_mascara(img, gaussiano_mascara, gaussiano_razao)
-    resultado_media = aplicar_mascara(img)
+    resultado_media = aplicar_mascara(img, media_mascara, media_razao)
 
     resultado = Image.fromarray(resultado)
     resultado.save("imagem/resultado.png")
