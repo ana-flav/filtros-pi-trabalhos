623d0da34608d8f2fcd91a8744ecba67156b351d
Ana Flavia Moreira Pires <flavia.pires@mail.uft.edu.br>
2024-10-17 22:07:52 -0300

    feat: :sparkles: hahah

--- filtros.py
+++ filtros.py
@@ -1,7 +1,29 @@
 import numpy as np
 
+def verificar_mascara_laplaciana(mascara):
+   
+    laplaciano_4_vizinhos_neg = np.array([[ 0,  1,  0],
+                                          [ 1, -4,  1],
+                                          [ 0,  1,  0]])
 
-def aplicar_mascara(imagem, mascara_matriz: list[list[int]], mascara_razao: int):
+    laplaciano_8_vizinhos_neg = np.array([[ 1,  1,  1],
+                                          [ 1, -8,  1],
+                                          [ 1,  1,  1]])
+
+    laplaciano_4_vizinhos_pos = np.array([[ 0,  1,  0],
+                                          [ 1,  4,  1],
+                                          [ 0,  1,  0]])
+
+    laplaciano_8_vizinhos_pos = np.array([[ 1,  1,  1],
+                                          [ 1,  8,  1],
+                                          [ 1,  1,  1]])
+
+    return (np.array_equal(mascara, laplaciano_4_vizinhos_neg) or 
+            np.array_equal(mascara, laplaciano_8_vizinhos_neg) or 
+            np.array_equal(mascara, laplaciano_4_vizinhos_pos) or 
+            np.array_equal(mascara, laplaciano_8_vizinhos_pos))
+
+def aplicar_mascara(imagem, mascara_matriz: list[list[int]], mascara_razao: int = 1) -> np.matrix:
     imagem = np.array(imagem)
     linhas, colunas = imagem.shape
     imagem = [[int(imagem[i][j]) for j in range(colunas)] for i in range(linhas)]
@@ -29,5 +51,16 @@ def aplicar_mascara(imagem, mascara_matriz: list[list[int]], mascara_razao: int)
             )
             novo_valor = max(0, round(novo_valor * mascara_razao))
             nova_matriz[i][j] = novo_valor
+
+        if verificar_mascara_laplaciana(np.array(mascara_matriz)):
+            imagem_original = np.array(imagem, dtype=np.int16) 
+            nova_matriz = imagem_original - nova_matriz
+
+            for i in range(linhas):
+                for j in range(colunas):
+                    if nova_matriz[i, j] < 0:
+                        nova_matriz[i, j] = 0  
+                    elif nova_matriz[i, j] > 255:
+                        nova_matriz[i, j] = 255  
 
     return np.matrix(nova_matriz).astype(np.uint8)
--- main.py
+++ main.py
@@ -12,11 +12,24 @@ def main():
     media_razao = 1 / 9  
 
 
+    laplaciano_mascara_1 = [[0, 1, 0], [1, -4, 1], [0, 1, 0]]
+    laplaciano_mascara_2 = [[1, 1, 1], [1, -8, 1], [1, 1, 1]]
+    laplaciano_mascara_3 = [[0, -1, 0], [-1, 4, -1], [0, -1, 0]]
+    laplaciano_mascara_4 = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
+
+
+    
     img = Image.open("imagem/imagem.png")
     img = img.convert("L")
 
     resultado = aplicar_mascara(img, gaussiano_mascara, gaussiano_razao)
     resultado_media = aplicar_mascara(img, media_mascara, media_razao)
+    resultado_laplaciano_1 = aplicar_mascara(img, laplaciano_mascara_1)
+    resultado_laplaciano_2 = aplicar_mascara(img, laplaciano_mascara_2)
+    resultado_laplaciano_3 = aplicar_mascara(img, laplaciano_mascara_3)
+    resultado_laplaciano_4 = aplicar_mascara(img, laplaciano_mascara_4)
+
+    
 
     resultado = Image.fromarray(resultado)
     resultado.save("imagem/resultado.png")
