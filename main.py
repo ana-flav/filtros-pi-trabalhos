5cb300825ca662a6c1f549778a068b682d595497
Ana Flavia Moreira Pires <flavia.pires@mail.uft.edu.br>
2024-10-17 22:22:36 -0300

    feat: :sparkles: main com os trem organizado

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 from PIL import Image
-
+import numpy as np
 from filtros import aplicar_mascara
 
+def salvar_imagem(resultado, nome_arquivo):
+    imagem = Image.fromarray(resultado)
+    imagem.save(nome_arquivo)
 
 def main():
-    # Mascara gaussiana
-    gaussiano_mascara = [[1, 2, 1], [2, 4, 2], [1, 2, 1]]
-    gaussiano_razao = 1 / 16
+    mascaras = {
+        "gaussiano": ([[1, 2, 1], [2, 4, 2], [1, 2, 1]], 1 / 16),
+        "media": ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], 1 / 9),
+        "laplaciano_1": ([[0, 1, 0], [1, -4, 1], [0, 1, 0]], 1),
+        "laplaciano_2": ([[1, 1, 1], [1, -8, 1], [1, 1, 1]], 1),
+        "laplaciano_3": ([[0, -1, 0], [-1, 4, -1], [0, -1, 0]], 1),
+        "laplaciano_4": ([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], 1)
+    }
 
-    media_mascara = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    media_razao = 1 / 9  
+    img = Image.open("imagem/imagem.png").convert("L")
+    img_array = np.array(img)
 
-
-    laplaciano_mascara_1 = [[0, 1, 0], [1, -4, 1], [0, 1, 0]]
-    laplaciano_mascara_2 = [[1, 1, 1], [1, -8, 1], [1, 1, 1]]
-    laplaciano_mascara_3 = [[0, -1, 0], [-1, 4, -1], [0, -1, 0]]
-    laplaciano_mascara_4 = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
-
-
-    
-    img = Image.open("imagem/imagem.png")
-    img = img.convert("L")
-
-    resultado = aplicar_mascara(img, gaussiano_mascara, gaussiano_razao)
-    resultado_media = aplicar_mascara(img, media_mascara, media_razao)
-    resultado_laplaciano_1 = aplicar_mascara(img, laplaciano_mascara_1)
-    resultado_laplaciano_2 = aplicar_mascara(img, laplaciano_mascara_2)
-    resultado_laplaciano_3 = aplicar_mascara(img, laplaciano_mascara_3)
-    resultado_laplaciano_4 = aplicar_mascara(img, laplaciano_mascara_4)
-
-    
-
-    resultado = Image.fromarray(resultado)
-    resultado.save("imagem/resultado.png")
-
+    for nome, (mascara, razao) in mascaras.items():
+        resultado = aplicar_mascara(img_array, mascara, razao)
+        salvar_imagem(resultado, f"imagem/resultado_{nome}.png")
 
 if __name__ == "__main__":
     main()
